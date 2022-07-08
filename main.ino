cf8aecfc05918b2f8bfaf25f9e5ff0d2fa6959b9
Gaurav Bansal <bansal22.gaurav@gmail.com>
2022-07-07 19:21:53 -0700

    done! its slightly janky and very incremental but it works. i tested it in unity engine :D

--- main.ino
+++ main.ino
@@ -1,3 +1,12 @@
+#include <Keyboard_da_DK.h>
+#include <Keyboard_fr_FR.h>
+#include <Keyboard_de_DE.h>
+#include <Keyboard_it_IT.h>
+#include <Keyboard_es_ES.h>
+#include <KeyboardLayout.h>
+#include <Keyboard_sv_SE.h>
+#include <Keyboard.h>
+
 /*
 unity is the best game engine asijdsaicndsgndsajsadngkdsatnasntn
 
@@ -17,46 +26,68 @@ void setup() {
   
   Serial.begin(9600);
   fin = "";
+  
+  Keyboard.begin();
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
 
-    
+
 }
 
 void loop() {
   
   fin = "";
-  
 
   //casdicsdijcwe8fjqwe8saiduqwddsajcsaedsasudfasudntidsqtndsnantas//psadc8dsck8fdakjdsgasjntdsan
   
   int sensorVal = analogRead(A0);
   int sensorVal2 = analogRead(A1);
+  Serial.println(sensorVal, DEC);
+  Serial.println(sensorVal2, DEC);
   
-  leftDist = abs(50 - sensorVal);
-  rightDist = abs(140-sensorVal);
+  leftDist = abs(0 - sensorVal);
+  rightDist = abs(1023-sensorVal);
   
   if (leftDist < 20 || rightDist < 20) {
     if (leftDist < rightDist) {
-      fin += "A";
+      
+      Keyboard.press('a');
+      delay(50);
+      Keyboard.release('a');
+   
+      Serial.println("mogusntiyengbeni");
     }
     else {
-      fin += "D";
+      
+      Keyboard.press('d');
+      delay(50);
+      Keyboard.release('d');
+    
     }
   }
   
   
-  topDist = abs(768 - sensorVal2);
-  bottomDist = abs(256-sensorVal2);
+  topDist = abs(0 - sensorVal2);
+  bottomDist = abs(1023-sensorVal2);
   
   if (topDist < 20 || bottomDist < 20) {
     if (topDist < bottomDist) {
-      fin += "W";
+    
+      Keyboard.press('w');
+      delay(50);
+      Keyboard.release('w');
+     
     }
     else {
-      fin += "S";
+      
+      Keyboard.press('s');
+      delay(50);
+      Keyboard.release('s');
+      
     }
   }
     
-  Serial.println(fin);
+ 
     
 
   
